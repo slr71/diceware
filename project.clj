6864fe6f4bb5c267b99daf53cdb6637de46d2866
Sarah Roberts <sarahr@cyverse.org>
2023-06-20 16:42:32 -0700

    update the package name and version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject diceware "0.1.0"
+(defproject org.clojars.slr71/diceware "0.0.1-SNAPSHOT"
   :description "A small utility to generate Diceware passwords."
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
